e14107bb1916c7f1ff186a2cecbf3808d8410375
Teetjy Boisnard <teetjyboisnard@hotmail.com>
2021-03-26 16:54:22 +0100

    26/03/21

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,30 @@
 var openmodal = document.querySelectorAll('.modal-open')
+
 for (var i = 0; i < openmodal.length; i++) {
     openmodal[i].addEventListener('click', function (event) {
         event.preventDefault()
-        toggleModal()
+
+        toggleModal(event.target.id)
     })
 }
+
 
 const overlay = document.querySelector('.modal-overlay')
 overlay.addEventListener('click', toggleModal)
 
+
 var closemodal = document.querySelectorAll('.modal-close')
 for (var i = 0; i < closemodal.length; i++) {
-    closemodal[i].addEventListener('click', toggleModal)
+    closemodal[i].addEventListener('click', function (event){
+        event.preventDefault()
+        toggleModal(event.target.id)
+    })
 }
 
-document.onkeydown = function (evt) {
-    evt = evt || window.event
-    var isEscape = false
-    if ("key" in evt) {
-        isEscape = (evt.key === "Escape" || evt.key === "Esc")
-    } else {
-        isEscape = (evt.keyCode === 27)
-    }
-};
 
-
-function toggleModal() {
+function toggleModal(id) {
     const body = document.querySelector('body')
-    const modal = document.querySelector('.modal')
+    const modal = document.querySelector('.modal-'+id)
     modal.classList.toggle('opacity-0')
     modal.classList.toggle('pointer-events-none')
     body.classList.toggle('modal-active')
